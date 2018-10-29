3d8107e6ca1207a34523bdec1ab1ed453c3a8dd9
david <2657070472@qq.com>
2018-10-29 13:52:05 +0800

    if语句，两数值比较

--- cs_lesson3.2/Program.cs
+++ cs_lesson3.2/Program.cs
@@ -10,6 +10,25 @@ namespace cs_lesson3._2
     {
         static void Main(string[] args)
         {
+            //if语句,比较两个数值大小
+            string comparison;
+            Console.WriteLine("Enter a number:");
+            double var1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number:");
+            double var2 = Convert.ToDouble(Console.ReadLine());
+            if (var1 < var2)
+                comparison = "less than";
+            else
+            {
+                if (var1 > var2)
+                    comparison = "greater than";
+                else
+                    comparison = "equal to";
+
+            }
+            Console.WriteLine("The first number is {0} the second number.", comparison );
+            Console.ReadKey();
+                                 
         }
     }
 }
